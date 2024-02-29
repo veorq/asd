3b816b8a1d04c2c2e69798731cf9cfc007b96a4e
JP Aumasson <jp@taurusgroup.ch>
2024-02-29 18:40:58 +0100

    asd

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# plumo
+# asd (a simple dataencryptiontool) 
+
+Boring straightforward password-based directory encryption. No options,
+no key management, just to quickly exchange encrypted files without
+having to dealing with GPG, OpenSSL, archivers' encryption, etc. 
+
+Encrypt a directory (AES128-GCM'd uncompressed tar archive):
+```
+asd <directory>
+``` 
+You'll be asked for a password, `directory` will be deleted.
+
+Decrypt a file (and extracting the tar archive):
+```
+asd <file>
+```
+You'll be asked for a password, `file` will be deleted.
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
 use std::env;
 use std::fs::{self, File, OpenOptions};
 use std::io::{self, Read, Write};
-use std::path::Path;
-use tar::Builder;
-use walkdir::WalkDir;
-use aes_gcm::{Aes256Gcm, aead::{Aead, NewAead, generic_array::GenericArray}};
-use argon2::{self, Config};
+use std::path::{Path, PathBuf};
+use tar::{Archive, Builder};
+use walkdir::{WalkDir, DirEntry};
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, generic_array::GenericArray},
+    Aes128Gcm, Key};
+use argon2::Argon2;
 use rpassword::read_password;
-use rand::{rngs::OsRng, RngCore};
+use rand::{rngs::OsRng};
+use zeroize::Zeroize;
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+         .to_str()
+         .map(|s| s.starts_with('.'))
+         .unwrap_or(false)
+}
 
 fn encrypt_file(file_path: &Path, password: &[u8]) -> io::Result<()> {
     let mut file = File::open(file_path)?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
 
-    // Derive a key from the password
-    let salt = rand::random::<[u8; 16]>(); // Generate a random salt
-    let config = Config::default();
-    let key = argon2::hash_raw(password, &salt, &config).unwrap();
-    let key = GenericArray::from_slice(&key);
+    let salt = rand::random::<[u8; 16]>(); 
+    let mut okm = [0u8; 16];
+    // yolo
+    let _ = Argon2::default().hash_password_into(password, &salt, &mut okm); 
+    let key = Key::<Aes128Gcm>::from_slice(&okm);
 
-    // Encrypt the file contents
-    let cipher = Aes256Gcm::new(key);
-    let nonce = rand::random::<[u8; 12]>(); // Generate a random nonce
-    let nonce = GenericArray::from_slice(&nonce);
-    let encrypted_data = cipher.encrypt(nonce, buffer.as_ref()).expect("encryption failure");
+    let cipher = Aes128Gcm::new(&key);
+    let noncega = Aes128Gcm::generate_nonce(&mut OsRng);
+    let ciphertext = cipher.encrypt(&noncega, buffer.as_ref()).expect("encryption failed");
+    okm.zeroize();
 
-    // Save the encrypted data to a new file
-    let mut encrypted_file = File::create(format!("{}.enc", file_path.display()))?;
-    encrypted_file.write_all(&nonce)?;
+    let mut encrypted_file = File::create(format!("{}", file_path.display()))?;
+    encrypted_file.write_all(&noncega)?;
     encrypted_file.write_all(&salt)?;
-    encrypted_file.write_all(&encrypted_data)?;
+    encrypted_file.write_all(&ciphertext)?;
 
     Ok(())
 }
@@ -42,57 +50,70 @@ fn decrypt_file(file_path: &Path, password: &[u8]) -> io::Result<()> {
     file.read_exact(&mut nonce)?;
     file.read_exact(&mut salt)?;
 
-    let mut encrypted_data = Vec::new();
-    file.read_to_end(&mut encrypted_data)?;
+    let mut ciphertext = Vec::new();
+    file.read_to_end(&mut ciphertext)?;
 
-    let config = Config::default();
-    let key = argon2::hash_raw(password, &salt, &config).unwrap();
-    let key = GenericArray::from_slice(&key);
+    let mut okm = [0u8; 16];
+    // yolo
+    let _ = Argon2::default().hash_password_into(password, &salt, &mut okm);
+    let key = Key::<Aes128Gcm>::from_slice(&okm);
 
-    let cipher = Aes256Gcm::new(key);
-    let nonce = GenericArray::from_slice(&nonce);
-    let decrypted_data = cipher.decrypt(nonce, encrypted_data.as_ref()).expect("decryption failure");
+    let cipher = Aes128Gcm::new(&key);
+    let noncega = GenericArray::from_slice(&nonce);
+    let plaintext = cipher.decrypt(noncega, ciphertext.as_ref()).expect("decryption failed");
+    okm.zeroize();
 
-    let mut decrypted_file = File::create(format!("{}.tar", file_path.display()))?;
-    decrypted_file.write_all(&decrypted_data)?;
+    let mut decrypted_file = File::create("asdtmp")?;
+    decrypted_file.write_all(&plaintext)?;
 
     Ok(())
 }
 
-fn secure_delete(file_path: &Path) -> io::Result<()> {
+fn zeroize_delete(file_path: &Path) -> io::Result<()> {
     if file_path.is_file() {
         let metadata = fs::metadata(file_path)?;
         let file_len = metadata.len();
-
         let mut file = OpenOptions::new().write(true).open(file_path)?;
         file.write_all(&vec![0; file_len as usize])?;
-        file.sync_all()?; // Ensure the write is flushed to disk
-
-        println!("rm {}", file_path.display());
+        file.sync_all()?; 
         fs::remove_file(file_path)?;
     }
 
     Ok(())
 }
 
-fn create_uncompressed_archive(dir_path: &Path, archive_path: &Path) -> io::Result<()> {
+fn create_archive(dir_path: &Path, archive_path: &Path) -> io::Result<()> {
     let file = File::create(archive_path)?;
     let mut archive = Builder::new(file);
 
-    for entry in WalkDir::new(dir_path) {
+    for entry in WalkDir::new(dir_path).into_iter().filter_entry(|e| !is_hidden(e)) {
         let entry = entry?;
         let path = entry.path();
         if path.is_file() {
-            // Adjust the path to be relative to dir_path
-            //let relative_path = path.strip_prefix(dir_path).unwrap();
             archive.append_path_with_name(path, path)?;
-            println!("process file {}", path.display());
-
-            // Securely delete the file
-            secure_delete(path)?;
+            zeroize_delete(path)?;
         } 
     }
     archive.finish()?;
+    Ok(())
+}
+
+fn extract_archive(archive_path: &Path, output_dir: &Path) -> io::Result<()> {
+    let file = File::open(archive_path)?;
+    let mut archive = Archive::new(file);
+
+    for file in archive.entries()? {
+        let mut file = file?;
+        let path = file.path()?;
+        let full_path = output_dir.join(path);
+
+        if let Some(parent) = full_path.parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+
+        file.unpack(&full_path)?;
+    }
+
     Ok(())
 }
 
@@ -103,38 +124,38 @@ fn main() -> io::Result<()> {
         None => Path::new("."),
     };
 
-    // Check if the provided path is a valid directory
     if cli_path.is_dir() {
-        println!("Enter password for encryption:");
-        let password = read_password().expect("Failed to read password");
-        println!("Confirm password:");
-        let confirm_password = read_password().expect("Failed to read password");
+        println!("password pls");
+        let mut password = read_password().expect("failed to read password");
+        println!("password again pls");
+        let confirm_password = read_password().expect("failed to read password");
 
         if password != confirm_password {
-            println!("Passwords do not match.");
+            println!("passwords dont match");
             return Ok(());
         }
 
-        // rename to ".plu"
-        let mut archive_path = PathBuf::from(dir_path);
-        archive_path.set_extension("plum");
+        let mut archive_path = PathBuf::from(cli_path);
+        archive_path.set_extension("asd");
 
-        create_uncompressed_archive(dir_path, archive_path)?;
-        fs::remove_dir_all(dir_path)?;
-        encrypt_file(archive_path, password)?;
-        secure_delete(archive_path)?;
+        create_archive(cli_path, archive_path.as_path())?;
+        fs::remove_dir_all(cli_path)?;
+        encrypt_file(archive_path.as_path(), password.as_bytes())?;
+        password.zeroize();
+
+    } else if cli_path.is_file() {
+        println!("password pls");
+        let password = read_password().expect("failed to read password");
+        decrypt_file(cli_path, password.as_bytes())?;
+        let archive = Path::new("./asdtmp");
+        let out_dir = Path::new("./");
+        extract_archive(archive, out_dir)?;
+        zeroize_delete(archive)?;
+        zeroize_delete(cli_path)?;
+    } else {
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "path is neither a valid directory nor a file"));
     }
-
-    if cli_path.is_file() {
-    let password = read_password().expect("Failed to read password");
-
-            decrypt_file(file_path, password.as_bytes())?;
-    }
-
-    return Err(io::Error::new(io::ErrorKind::InvalidInput, "Provided path is neither a valid directory nor a file."));
-
 
     Ok(())
 }
-
 
